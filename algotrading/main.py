f364fa18550f2628dfdb2298561c65280b43dc7c
stefanyeunghk <40346977+stefanyeunghk@users.noreply.github.com>
2021-06-17 11:14:48 +0800

    Add files via upload

---
+++ algotrading/main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from selenium import webdriver
+import requests as rq
+import xlsxwriter
+import math
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
+from urllib.request import Request
+import numpy as np
+
+apple_url = "https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch"
+
+uClient = uReq(apple_url)
+result_html = uClient.read()
+uClient.close()
+
+page_soup = soup(result_html, "html.parser")
+result = page_soup.findAll("span", {"class": "Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)"})
+
+# target span is <span class="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)" data-reactid="33">+0.51 (+0.39%)</span>
+
+
