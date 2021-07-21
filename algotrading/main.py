4616e4ad711801487891df543876be29d55be1a4
Stefan yeung <stefan.yeung@onedegree.hk>
2021-07-21 17:39:18 +0800

    add necessary files

--- algotrading/main.py
+++ algotrading/main.py
@@ -9,14 +9,16 @@ from urllib.request import Request
 import numpy as np
 
 apple_url = "https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch"
-
+print(apple_url)
 uClient = uReq(apple_url)
 result_html = uClient.read()
 uClient.close()
 
 page_soup = soup(result_html, "html.parser")
+print(page_soup)
 result = page_soup.findAll("span", {"class": "Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)"})
-
-# target span is <span class="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)" data-reactid="33">+0.51 (+0.39%)</span>
+print(result)
+# target span is <span class="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)" data-reactid="33">+0.51 (
+# +0.39%)</span>
 
 
